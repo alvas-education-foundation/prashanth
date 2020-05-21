ca1f6ca3856fa01f8c60ad5958ca2e9f36551637
prashanth <32684496+prashanths8199@users.noreply.github.com>
2020-05-21 16:55:34 +0530

    Create rotate.py

---
+++ coding_solutions/rotate.py
@@ -0,0 +1,12 @@
+def right_rotate(l,n):
+    result=[]
+    for i in range(len(l)-n,len(l)):
+        result.append(l[i])
+    for j in range(0,len(l)-n):
+        result.append(l[j])
+    return result
+
+l=[]
+l=int(input("Enter the array to rotate"))
+k=int(input('enter number of times to right rotate:'))
+print(right_rotate(l,k))
