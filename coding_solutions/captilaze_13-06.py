2c1be5519d0689b9e741744aee254994248c9b4f
prashanth <32684496+prashanths8199@users.noreply.github.com>
2020-06-13 19:31:51 +0530

    13/06

---
+++ coding_solutions/captilaze_13-06.py
@@ -0,0 +1,4 @@
+test_str = input()
+print("The original string is : " + str(test_str)) 
+res = test_str[0].upper() + test_str[1:]   
+print("The string after uppercasing initial character : " + str(res))
